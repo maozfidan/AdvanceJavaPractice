cf3313b52b05fe4b60c6a5e1a010ccddb400e271
M.Arif Özfidan <ozfidanarif@gmail.com>
2022-04-12 09:37:04 +0200

    12.04.2022

---
+++ src/JavaProjects/P04_okulYönetimi/ogrcOgrtYonetimi/Runner.java
@@ -0,0 +1,19 @@
+package JavaProjects.P04_okulYönetimi.ogrcOgrtYonetimi;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+     // static  ArrayList<Kisi> ogrncistesi=new ArrayList<>();
+     // static  ArrayList<Kisi> ogrtmciistesi=new ArrayList<>();
+     // static  Scanner scan =new Scanner (System.in);
+     // static  String kisiTuru;
+
+
+
+
+    }
+}
