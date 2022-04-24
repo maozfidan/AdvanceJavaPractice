ee7cb5504acce4fd629f6bf0786ccd6822a68b8a
M.Arif Özfidan <ozfidanarif@gmail.com>
2022-04-24 18:40:55 +0200

    24.04.2022

--- src/JavaProjects/P04_okulYönetimi/Course.java
+++ src/JavaProjects/P04_okulYönetimi/Course.java
@@ -2,7 +2,7 @@ package JavaProjects.P04_okulYönetimi;
 
 public class Course {
 
-    JavaProjects.P04_OkulYönetimi.Teacher teacher;
+    //JavaProjects.P04_OkulYönetimi.Teacher teacher;
     String name;
     String prefix;
     int noteYazili;
